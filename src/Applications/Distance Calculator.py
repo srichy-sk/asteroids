cdba36369c3c3df8a05ddcef9f0fd2b63575e976
srichy-sk <srichy.sk@gmail.com>
2026-06-25 13:49:48 -0700

    Added Disntance calculator

---
+++ src/Applications/Distance Calculator.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+#from tkinter import ttk
+
+def convert():
+    mile_input = entry_int .get()
+    km_output = mile_input * 1.60934
+    output_string.set(km_output)
+
+#window
+window = ttk.Window(themename = 'journal')
+window.title('Demo')
+window.geometry('350x150')
+
+#title
+title_label = ttk.Label(master = window, text = 'Miles To Kilometers', font = 'Calibri 24 bold')
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master = window)
+entry_int = tk.IntVar()
+
+entry = ttk.Entry(master = input_frame, textvariable = entry_int)
+button = ttk.Button(master = input_frame, text = 'Convert', command = convert)
+
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+#output
+output_string = tk.StringVar()
+
+output_label = ttk.Label(master = window,
+                         text = 'output',
+                         font = 'Calibri 16 ',
+                         textvariable = output_string)
+output_label.pack(pady = 5)
+
+#run
+window.mainloop()
